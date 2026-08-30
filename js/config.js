d0395e7783561e46b5cc669dc47d36c684319ae4
fitar50 <habd69185@gmail.com>
2026-08-30 11:59:44 +0300

    Last Sync: 2026-08-30 11:59 (Mobile)

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
 // CONFIGURATION
 // ================================================================
 // Fill in your Apps Script URL after deploying
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbymmQbfF0cm_35OszzUyrAPtKmEhJoCkrMpiIEB-5PTZo-acyIXVG5L3UxRXkY07p0R/exec;
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbymmQbfF0cm_35OszzUyrAPtKmEhJoCkrMpiIEB-5PTZo-acyIXVG5L3UxRXkY07p0R/exec';
 const MGR_PARAM  = 'backstage';
 const DELIVERY_FEE = 30;
