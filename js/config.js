9e9253a8e1d8115c039033edfa41ea57fe1908db
fitar <habd69185@gmail.com>
2026-09-04 15:00:01 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-// ================================================================
-// CONFIGURATION
-// ================================================================
-// Fill in your Apps Script URL after deploying
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwv3DSP7dBVFSRWYWqF_-eDRpcPq6bQmGegFkHb3dtsh7WlTU123tsZDVYU6N6uCDqRzg/exec';
-const MGR_PARAM  = 'backstage';
-const DELIVERY_FEE = 30;
